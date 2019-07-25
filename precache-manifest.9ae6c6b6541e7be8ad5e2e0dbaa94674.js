6803bbd7a187a97637b5b92f8711f261c1b13e05
Namrata Subhedar <nammsy@gmail.com>
2019-07-25 20:52:31 +0530

    Updates

---
+++ precache-manifest.9ae6c6b6541e7be8ad5e2e0dbaa94674.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e5fe38e78ea56385630b12029844e2b",
+    "url": "/clothingReact/index.html"
+  },
+  {
+    "revision": "fde556421517505c47d2",
+    "url": "/clothingReact/static/css/main.76cbfb45.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/clothingReact/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "fde556421517505c47d2",
+    "url": "/clothingReact/static/js/main.f4784c4e.chunk.js"
+  },
+  {
+    "revision": "356ea0beff46bb084e2f",
+    "url": "/clothingReact/static/js/runtime~main.8e393b49.js"
+  }
+]);
